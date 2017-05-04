7e0425ec8ae5cec078d013a015c67dd0906ff31e
Gary Martin <gary@labworkz.com>
2017-05-04 14:23:06 -0500

    Update to application

---
+++ README.md
@@ -0,0 +1,30 @@
+## How to use this template
+
+*This template does not work on its own*. The shared files for each starter are found in the [ionic2-app-base repo](https://github.com/driftyco/ionic2-app-base).
+
+To use this template, either create a new ionic project using the ionic node.js utility, or copy the files from this repository into the [Starter App Base](https://github.com/driftyco/ionic2-app-base).
+
+### With the Ionic CLI:
+```bash
+	$ sudo npm install
+```
+
+### Android
+
+```bash
+	$ ionic platform add android
+	$ ionic build android --prod
+	$ ionic run android --prod
+```
+
+### iOS
+```bash
+	$ ionic platform add ios
+	$ ionic build ios --prod
+```    
+    Run using XCode
+
+### Generate Icons
+```bash
+	$ ionic resources
+```
---
+++ src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public icons;
+  public items;
+
+  constructor(public navCtrl: NavController) {
+
+
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+      'american-football', 'boat', 'bluetooth', 'build'];
+
+    this.items = [];
+    for (let i = 1; i < 100; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)],
+        image: "https://s3.amazonaws.com/busites_www/woodlandscenter2016com/pages/Casting-Crowns.jpg"
+      });
+    }
+  }
+
+}
